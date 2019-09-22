0e5c2bcbcf9787a27151bb85aba4d0722ad5cc5c
timac11 <timac11@bk.ru>
2019-09-22 10:52:07 +0300

    commit for login page

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './dashboard/main'
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
+import {ThemeProvider} from '@material-ui/styles';
 import theme from "./theme/theme";
-
-const title = 'Hello, world!';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Login from "./dashboard/login";
 
 ReactDOM.render(
-			<ThemeProvider theme={theme}>
-                <CssBaseline />
-				<Main/>
-			</ThemeProvider>,
-	document.getElementById('app')
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Router>
+            <Switch>
+                <Route path='/wizard' component={Main}/>
+                <Route path='/login' component={Login}/>
+            </Switch>
+        </Router>
+    </ThemeProvider>,
+    document.getElementById('app')
 );
